b3141889e991002cc48beae23ce992da601d0dee
Juan David Lozano <48156707+Itsudka@users.noreply.github.com>
2024-07-21 21:37:57 -0500

    Single posts

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,9 +1,40 @@
+import { client } from "@/sanity/lib/client";
+import Header from "../components/Header";
+import { Post } from "../utils/Interface";
+import PostComponent from "../components/PostComponent";
 
+async function getPosts() {
+  const query = `
+    *[_type == "post"] {
+  title,
+  slug,
+  publishedAt,
+  excerpt,
+  tags[]->{
+    _id,
+    slug,
+    name
+  }
+}
+  `;
 
-export default function Home() {
+  const data = await client.fetch(query);
+  return data;
+}
+
+export const revalidate = 60;
+
+export default async function Home() {
+  const posts: Post[] = await getPosts()
+  console.log(posts, 'posts')
   return (
     <div>
-      <h1>Hello world</h1>
+      <Header title="Articles"/>
+      <div>
+        {posts.length > 0 && posts?.map((post) => (
+          <PostComponent key={post._id} post={post}/>
+        ))}
+      </div>
     </div>
   );
 }
